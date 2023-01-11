f05e951a4a7701b375a93ae91025d2f041969409
pandasun92 <sonrichard355@gmail.com>
2023-01-10 19:25:35 -0800

    added javascript

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign ='Left'
+let dogImages = document.querySelectorAll('.dog-image')
 
-// Select some elements...
+for(let i=0; i<dogImages.length; i++){
+    dogImages[i].style.borderRadius='50px'
+    dogImages[i].style.rotate = '180deg'
+}
+
+
+let dogNames=document.querySelectorAll('.dog-name')
+for(let i=0; i<dogNames.length;i++){
+    dogNames[i].style.textAlign = 'Left'
+}
+let footer=document.querySelector('.footer')
+footer.style.color='blue'
